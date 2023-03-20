460dba3679a079a93f1acda8337fd714059e33f6
Guillaume Douv <gui.douvrin@gmail.com>
2023-03-20 16:07:09 +0100

    Import styles.css et création d'un boucle for pour intégrer tous les élements de la liste

--- pieces.js
+++ pieces.js
@@ -2,32 +2,50 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-// Création des balises
-const article = pieces[0];
+for (let i = 0; i < pieces.length; i++) {
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // On crée l’élément img.
+    const imageElement = document.createElement("img");
+    // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
+    imageElement.src = pieces[i].image;
+    // Idem pour le nom, le prix et la catégorie...
+    // Nom
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = pieces[i].nom;
+    // Prix
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${pieces[i].prix} € (${
+        pieces[i].prix < 35 ? "€" : "€€€"
+    })`;
+    // Categorie
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+    // Description
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText =
+        pieces[i].description ?? "Pas de description pour le moment.";
+    // Disponibilité
+    const disponibiliteElement = document.createElement("p");
+    disponibiliteElement.innerText = pieces[i].disponibilite
+        ? "En stock"
+        : "Rupture de stock";
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${
-    article.prix < 35 ? "€" : "€€€"
-})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText =
-    article.description ?? "Pas de description pour le moment.";
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite
-    ? "En stock"
-    : "Rupture de stock";
-
-//Rattachement de nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibiliteElement);
+    // On rattache la balise article à la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    // Idem pour le nom, le prix et la catégorie...
+    // Nom
+    pieceElement.appendChild(nomElement);
+    // Prix
+    pieceElement.appendChild(prixElement);
+    // Catégorie
+    pieceElement.appendChild(categorieElement);
+    // Description
+    pieceElement.appendChild(descriptionElement);
+    // Disponibilité
+    pieceElement.appendChild(disponibiliteElement);
+}
